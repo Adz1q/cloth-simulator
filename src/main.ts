d4a0d662a98c6a46da3440c3235ad4f45d6122bb
Adz1q <mikolajs2211@gmail.com>
2026-01-08 21:14:42 +0100

    Add flying cube

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,61 @@
+import { Point } from './point';
 import './style.css';
-import typescriptLogo from './typescript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.ts';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+const canvas = document.getElementById('canvas') as HTMLCanvasElement | null;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+if (!canvas) {
+  throw new Error('Canvas element not found');
+}
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
+document.body.appendChild(canvas);
+
+const ctx = canvas.getContext('2d');
+
+if (!ctx) {
+  throw new Error('2D context not supported or canvas already initialized');
+}
+
+ctx.fillStyle = 'green';
+const point = new Point(canvas.width / 2, 1);
+
+// Point Size
+const pointWidth = 10;
+const pointHeight = 10;
+
+// Positions
+let oldY: number;
+
+// Parameters
+const g = 9.81;
+const a = g;
+let v = 0;
+const frameMs = 10;
+const dt = frameMs / 1000;
+
+function calculatePosition(point: Point) {
+  oldY = point.y;
+  v += a;
+  point.y = point.y + v * dt + 0.5 * a * dt * dt;
+}
+
+function render(point: Point) {
+  calculatePosition(point);
+  ctx.clearRect(point.x, oldY, pointWidth, pointHeight);
+  ctx.fillRect(point.x, point.y, pointWidth, pointHeight);
+}
+
+setInterval(() => {
+  render(point);
+}, frameMs);
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth - canvasOffsetX;
+  canvas.height = window.innerHeight - canvasOffsetY;
+});
+
+// calculate new cords and old cords, remove old point, draw new point, repeat
